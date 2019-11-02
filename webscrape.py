2ba721b61221470284c4264d204397cb44003c26
Daniel Raad <dr1g18@soton.ac.uk>
2019-11-02 01:07:17 +0000

    Deadlines Cambridge scrape
    Removed Comment

--- webscrape.py
+++ webscrape.py
@@ -3,38 +3,6 @@ from bs4 import BeautifulSoup
 import csv
 
 #
-# URL = "https://www.undergraduate.study.cam.ac.uk/events/cambridge-open-days"
-# r = requests.get(URL)
-# soup = BeautifulSoup(r.content, 'html5lib')
-# deadlineDates = []
-# #print(soup.prettify())
-#
-# table = soup.find('head')
-# # print(table.prettify())
-#
-# # for row in table.findAll('meta', attrs={'name', 'description'}):
-# #     print(row)
-#
-# row = soup.find('meta', attrs={'name': 'description'})
-# print(row['content'])
-# deadlineDates['Cambridge'] = row['content']
-#
-#
-# # for row in table .findAll('meta', attrs = {'name': 'dcterms.description'}):
-# #         deadlineDates = {}
-# #         deadlineDates['Cambridge'] = row.content
-#
-#
-# print(deadlineDates)
-#
-#
-
-# URL = "https://www.undergraduate.study.cam.ac.uk/applying/ucas-application"
-# r = requests.get(URL)
-# soup = BeautifulSoup(r.content, 'html5lib')
-# #print(soup.prettify())
-# row = soup.find('meta', attrs={'name': 'description'})
-# print(row['content'])
 
 
 URL = "https://www.ucas.com/events/2020-entry-deadline-universities-oxford-and-cambridge-and-most-courses-medicine-veterinary-348816"
@@ -44,10 +12,3 @@ soup = BeautifulSoup(r.content, 'html5lib')
 row = soup.find('meta', attrs={'name': 'expires'})
 print(row['content'])
 
-
-# URL = "https://www.undergraduate.study.cam.ac.uk/courses/computer-science"
-# r = requests.get(URL)
-# soup = BeautifulSoup(r.content, 'html5lib')
-# grades = []
-# print(soup.prettify())
-# print(soup.find('id'='entry-requirements'))
