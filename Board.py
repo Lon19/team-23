fd9fdc4e7b04517de989a09f824c6c745c84065d
Daniel Raad <dr1g18@soton.ac.uk>
2019-11-02 10:29:14 +0000

    test page puller

--- Board.py
+++ Board.py
@@ -1,4 +1,4 @@
-import pickle
+port pickle
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -8,10 +8,9 @@ class Basket:
 
     def __init__(self):
         self.mylist = self.reload()
+        if len(self.mylist) >= 3:
+            self.removeboard(self.mylist[0])
 
-    def tradeboard(self, newboard, currentboard):
-        self.removeboard(currentboard)
-        self.addboard(newboard)
 
     def save(self):
         with open(r'persistence.pkl', 'wb') as f:
@@ -38,25 +37,15 @@ class Board:
         self.keyDates = kwargs
 
 
-
-
 class Pagepuller:
 
     def __init__(self):
         pass
 
-    def requirements(self, url):
-        URL = url
-        r = requests.get(URL)
-        print("The entry requirements for " + input_course + " are:")
-        soup = BeautifulSoup(r.content, 'html.parser')
+    def createBoard(self, university):
+        board = Board(university, {'deadline': self.deadline()})
+        return board
 
-        temp = [td.find_next('p') for td in soup.find(id="entry-requirements")]
-        requirement = temp[0].get_text()
-        a_level, ib = requirement.split("IB")
-        print (a_level)
-        ib = requirement.replace(a_level, '')
-        print(ib)
 
     def deadline(self):
         URL = "https://www.ucas.com/events/2020-entry-deadline-universities-oxford-and-cambridge-and-most-courses-medicine-veterinary-348816"
@@ -71,26 +60,41 @@ class Pagepuller:
 
 
 
+def parser(a_level, ib):
+    message = """<html>
+    <head></head>
+    <p>The Requirements for Cambridge University</p>
+    <p>"{0}"</p>
+    <p>"{1}"</p>
+    </html>
+    """.format(a_level, ib)
+    print(message)
 
 
-page = Pagepuller()
-basket = Basket()
-basket.addboard(page.createBoard("Cambridge"))
-print(basket.mylist)
+def requirements(url):
+    URL = url
+    r = requests.get(URL)
+    print("The entry requirements for " + " are:")
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    temp = [td.find_next('p') for td in soup.find(id="entry-requirements")]
+    requirement = temp[0].get_text()
+    a_level, ib = requirement.split("IB")
+    print(a_level)
+    ib = requirement.replace(a_level, '')
+    print(ib)
+    return parser(a_level=a_level, ib=ib)
 
 
+def get_requirements():
+    course_hashmap = {"Computer Science": "https://www.undergraduate.study.cam.ac.uk/courses/computer-science",
+                      "Chemical Engineering": "https://www.undergraduate.study.cam.ac.uk/courses/chemical-engineering",
+                      "Mathematics": "https://www.undergraduate.study.cam.ac.uk/courses/mathematics"
+                      }
+    input_course = "Mathematics"
+    return requirements(course_hashmap.get(input_course))
 
 
-course_hashmap = {"Computer Science": "https://www.undergraduate.study.cam.ac.uk/courses/computer-science",
-                  "Chemical Engineering": "https://www.undergraduate.study.cam.ac.uk/courses/chemical-engineering",
-                  "Mathematics": "https://www.undergraduate.study.cam.ac.uk/courses/mathematics"
-                  }
-input_course = "Mathematics"
-page.requirements(course_hashmap.get(input_course))
-
-
-<<<<<<< Updated upstream
-=======
 def test_page():
     page = Pagepuller()
     basket = Basket()
@@ -100,4 +104,3 @@ def test_page():
 
 
 test_page()
->>>>>>> Stashed changes
