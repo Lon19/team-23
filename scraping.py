4e9b813ce6be1fa8bfd65ded152e9e9d60772cc8
Daniel Raad <dr1g18@soton.ac.uk>
2019-11-02 10:15:16 +0000

    fixed bugs

--- scraping.py
+++ scraping.py
@@ -1,21 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-def parser(a_level,ib):
-    message = """<html>
+def parser(a_level,ib):
+    message="""<html>
     <head></head>
     <p>The Requirements for Cambridge University</p>
     <p>"{0}"</p>
     <p>"{1}"</p>
     </html>
-    """.format(a_level, ib)
-    return message
+    """.format(a_level,ib)
+    print(message)
 
 def requirements(url):
     
     URL = url
     r = requests.get(URL)
-    print("The entry requirements for " + input_course + " are:")
+    print("The entry requirements for " + " are:")
     soup = BeautifulSoup(r.content, 'html.parser')
 
     temp = [td.find_next('p') for td in soup.find(id="entry-requirements")]
@@ -33,3 +33,5 @@ def get_requirements():
     }
     input_course = "Mathematics"
     return requirements(course_hashmap.get(input_course))
+
+get_requirements()
